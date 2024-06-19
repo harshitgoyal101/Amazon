c4a089ca7826e7d78c45ad94f6a022294ca3f202
harshitgoyal101 <harshitgoyal101@gmail.com>
2024-06-19 23:25:00 +0530

    some

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
