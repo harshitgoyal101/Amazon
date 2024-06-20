4b891d1615eede49b7a9c3fb2aceddef68cc2ba5
harshitgoyal101 <harshitgoyal101@gmail.com>
2024-06-21 00:01:28 +0530

    ApiDocs

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import ApiDoc
 
 # Register your models here.
+admin.site.register(ApiDoc)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class ApiDoc(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.TextField()
+    response = models.JSONField()
+    request = models.JSONField()
+
+    def __str__(self):
+        return self.name
